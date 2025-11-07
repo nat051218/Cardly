247418a65e92f1d980326b1a03b6a27863063ff9
Natalee C <nataleechen1218@gmail.com>
2025-11-07 09:14:00 +0800

    Initial Commit

---
+++ w10_lesson/w10_lessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  w10_lessonApp.swift
+//  w10_lesson
+//
+//  Created by natalee chen on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct w10_lessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
